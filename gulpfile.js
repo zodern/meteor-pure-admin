3d29e2a4cc952788057869c4261057eaede5a1bd
zodern <zodern@users.noreply.github.com>
2015-09-09 19:25:59 -0500

    rename files to something consistent

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 var gulp = require('gulp');
 var child = require('child_process');
+var fs = require('fs');
+var path = require('path');
 
 gulp.task('build-client', function() {
   var cwd = __dirname + '/src';
-  child.exec(__dirname + '/node_modules/meteor-build-client/main.js ../output ', {cwd: cwd}, function (err, result) {
-    console.log(err);
-    console.log(result);
+  var result = child.execSync(__dirname + '/node_modules/meteor-build-client/main.js ../output ', {cwd: cwd});
+  console.log(result.toString('binary'));
+  // rename files
+  var fileList = fs.readdirSync(__dirname + '/output');
+  console.log(fileList);
+  fileList.forEach(function (item) {
+    var ext = path.extname(item);
+    if(ext === 'map') {
+      return;
+    }
+    var base = __dirname + '/output/';
+    fs.renameSync(base + item, base + 'pure-admin' + ext );
   });
 });
