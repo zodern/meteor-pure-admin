5f9939532dde5c9261745142aa8a8f0f16f514d5
zodern <zodern@users.noreply.github.com>
2015-09-27 20:33:23 -0500

    add dash in name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'zodern:pureadmin',
+  name: 'zodern:pure-admin',
   version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Isolated, customizable admin panel for Meteor',
