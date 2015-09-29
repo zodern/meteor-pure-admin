9bc45d5ba5b4773d452f9a65798435e1ab7c1a17
zodern <zodern@users.noreply.github.com>
2015-09-29 13:19:56 -0500

    Updated version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zodern:pure-admin',
-  version: '0.1.1',
+  version: '0.1.2',
   // Brief, one-line summary of the package.
   summary: 'Isolated, customizable admin panel for Meteor',
   // URL to the Git repository containing the source code for this package.
