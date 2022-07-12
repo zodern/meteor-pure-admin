f2fdd9d0558b9a115f103c05e49be33b51ff62b7
zodern <zodern@users.noreply.github.com>
2022-07-12 11:15:45 -0500

    v0.9.0-beta.1

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'zodern:pure-admin',
-  version: '0.8.3',
+  version: '0.9.0-beta.1',
   summary: 'Isolated, customizable admin panel for Meteor',
   git: 'https://github.com/zodern/meteor-pure-admin.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.8.0.2');
-  api.use(['typescript', 'ecmascript', 'dynamic-import']);
+  api.versionsFrom('1.8.2');
+  api.use(['typescript@3.7.0||4.0.0', 'ecmascript', 'dynamic-import']);
   api.use('zodern:melte@1.4.0');
-  api.use('zodern:types');
+  api.use('zodern:types@1.0.1');
 
   api.mainModule('./src/main.ts', 'client');
   api.mainModule('./src/server.js', 'server');
