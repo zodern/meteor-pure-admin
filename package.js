e1da3016054025a39c42bd39015e4cde95975ee6
zodern <zodern@users.noreply.github.com>
2023-06-08 11:13:55 -0500

    v0.9.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zodern:pure-admin',
-  version: '0.9.0',
+  version: '0.9.1',
   summary: 'Isolated, customizable admin panel for Meteor',
   git: 'https://github.com/zodern/meteor-pure-admin.git',
   documentation: 'README.md'
